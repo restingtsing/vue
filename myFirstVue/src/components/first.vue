60ed23758ef4506f855abfc4200e24a986c682b4
RESTING <1262197441@qq.com>
2019-04-28 13:39:02 +0800

    this is my firstVue

---
+++ myFirstVue/src/components/first.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="color:red"><h1 v-text="msg"></h1></div>
+</template>
+<script>
+export default {
+    name:'first',
+    data(){
+        return{
+            msg:"this is first page"
+        }
+    }
+}
+</script>
