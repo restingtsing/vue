a50f4a9d35a0a3859586e52f6d8109b7ead658b6
RESTING <1262197441@qq.com>
2019-06-06 14:40:34 +0800

    20190606

--- music_pro/build/webpack.dev.conf.js
+++ music_pro/build/webpack.dev.conf.js
@@ -48,7 +48,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     watchOptions: {
       poll: config.dev.poll,
     },
-    before(app){
+    before(app){ 
       app.get('/api/getList',(req,res)=>{
         const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
         axios.get(url,{
@@ -63,6 +63,20 @@ const devWebpackConfig = merge(baseWebpackConfig, {
           console.log(e)
         })
       })
+      // app.get('/api/getSinger',(req,res)=>{
+      //   const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+      //   axios.get(url,{
+      //     headers:{
+      //       referer:'https://y.qq.com',
+      //       host:'c.y.qq.com'
+      //     },
+      //     params:req.query
+      //   }).then((response)=>{
+      //     res.json(response.data)
+      //   }).catch((e)=>{
+      //     console.log(e)
+      //   })
+      // })
     }
   },
   plugins: [
--- music_pro/src/components/singer/singer.vue
+++ music_pro/src/components/singer/singer.vue
@@ -1,15 +1,87 @@
 <template>
-    <div>
-        歌手页面
+    <div class="singer" ref="singer">
+        <ListViwe :data="singerList"></ListViwe>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-export default {
-    
+import {getSingerList} from '../../api/singer'
+import {ERR_OK} from '../../api/config'
+import Singer from '../../common/js/singer'
+import ListViwe from '../../base/listview/listview'
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
+export default{
+    data(){
+        return{
+            singerList:[]
+        }
+    },
+    created(){
+        this._getSingerList()
+    },
+    components:{ListViwe},
+    methods:{
+        _getSingerList(){
+            getSingerList().then((res)=>{
+                if(res.code===ERR_OK){
+                    this.singerList =this._normalize(res.data.list)
+                    console.log(res.data.list)
+                }
+            })
+        },
+        _normalize(list){
+            let map = {
+                hot:{
+                    title:HOT_NAME,
+                    items:[]
+                }
+            }
+            list.forEach((item,index) => {
+                if(index < HOT_SINGER_LEN){
+                    map.hot.items.push(new Singer({
+                        name:item.Fsinger_name,
+                        id:item.Fsinger_mid
+                    }))
+                }
+                const key = item.Findex
+                if(!map[key]){
+                    map[key] = {
+                       title:key,
+                       items:[]
+                   }
+                }
+                map[key].items.push(new Singer({
+                    name:item.Fsinger_name,
+                    id:item.Fsinger_mid
+                }))
+            })
+            //有序列表
+            let ret = []
+            let hot = []
+            for(let key in map){
+                let val = map[key]
+                if(val.title.match(/[a-zA-Z]/)){
+                    ret.push(val)
+                }else if(val.title == HOT_NAME){
+                    hot.push(val)
+                }
+            }
+            ret.sort((a,b)=>{
+                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+            })
+            return hot.concat(ret)
+        }
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+    .singer
+        position :fixed
+        top :88px
+        bottom :0
+        width :100%
 </style>
